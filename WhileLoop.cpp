832af31a310eb3a0c9d60a12f6e006229019a3e0
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:21:57 +0530

    Program to demonstrate WHILE LOOP in cpp

---
+++ WhileLoop.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate WHILE LOOP in cpp.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    cout<<"\n\n\t Demonstration of WHILE LOOP in C++ >> ";
+    while(i <= 5){
+        cout<<" "<<i++;
+    }
+    return 0;
+}
+
